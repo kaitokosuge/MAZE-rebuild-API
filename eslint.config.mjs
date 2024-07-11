c13dc5626f782e27d82f7f4325d46dd0e7a415ae
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-07-11 13:17:19 +0900

    api start

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+	{
+		files: ["**/*.{js,mjs,cjs,ts}"],
+		rules: {
+			"@typescript-eslint/naming-convention": [
+				"error",
+				{
+					selector: "variable",
+					format: ["camelCase"],
+				},
+				{
+					selector: "interface",
+					format: ["PascalCase"],
+				},
+				{
+					selector: "typeAlias",
+					format: ["PascalCase"],
+				},
+				{
+					selector: "typeParameter",
+					format: ["camelCase"],
+				},
+			],
+			"no-console": ["error"],
+		},
+	},
+	{
+		languageOptions: { globals: globals.browser },
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+];
