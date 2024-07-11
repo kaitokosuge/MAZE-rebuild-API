ce4ee44148c6264a23c7ee89ba8455840c94b0a4
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-07-11 16:41:02 +0900

    add: フォーマットコマンド追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,34 +3,34 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-	{
-		files: ["**/*.{js,mjs,cjs,ts}"],
-		rules: {
-			"@typescript-eslint/naming-convention": [
-				"error",
-				{
-					selector: "variable",
-					format: ["camelCase"],
-				},
-				{
-					selector: "interface",
-					format: ["PascalCase"],
-				},
-				{
-					selector: "typeAlias",
-					format: ["PascalCase"],
-				},
-				{
-					selector: "typeParameter",
-					format: ["camelCase"],
-				},
-			],
-			"no-console": ["error"],
-		},
-	},
-	{
-		languageOptions: { globals: globals.browser },
-	},
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        rules: {
+            "@typescript-eslint/naming-convention": [
+                "error",
+                {
+                    selector: "variable",
+                    format: ["camelCase"],
+                },
+                {
+                    selector: "interface",
+                    format: ["PascalCase"],
+                },
+                {
+                    selector: "typeAlias",
+                    format: ["PascalCase"],
+                },
+                {
+                    selector: "typeParameter",
+                    format: ["camelCase"],
+                },
+            ],
+            "no-console": ["error"],
+        },
+    },
+    {
+        languageOptions: { globals: globals.browser },
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
 ];
