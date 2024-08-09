fa0503bd2b1cb6a2da230e56056f5cee946a2a76
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-08-09 21:27:51 +0900

    add: frontと連携確認完了

--- app.ts
+++ app.ts
@@ -1,11 +1,19 @@
-import express from "express";
+import express, { Request } from "express";
+import cors from "cors";
 const app = express();
-const port = 3000;
+const port = 3001;
 
+app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello World!As");
 });
 
+app.post("/post/admin-quiz", async (req: Request, res) => {
+    res.send("hello next");
+    console.log("requestです", req.body);
+});
+
 app.listen(port, () => {
-    //console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
